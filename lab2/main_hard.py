3ed69b23c47f4253102a30e8d037e86d9affcfff
SilverMoon17 <50795723+SilverMoon17@users.noreply.github.com>
2023-10-10 15:32:41 +0300

    commit lab2

--- lab2/main_hard.py
+++ lab2/main_hard.py
@@ -2,16 +2,17 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
+import time;
 
 # Создаем папку dataset, если она не существует
-if not os.path.exists('dataset'):
-    os.mkdir('dataset')
+if not os.path.exists('dataset_hard'):
+    os.mkdir('dataset_hard')
 
 
 # Функция для загрузки изображений и сохранения их в подпапке dataset
 def download_images(search_query, class_name, num_images=1000):
     # Создаем подпапку с соответствующим именем
-    folder_path = os.path.join('dataset', class_name)
+    folder_path = os.path.join('dataset_hard', class_name)
     if not os.path.exists(folder_path):
         os.mkdir(folder_path)
 
@@ -21,21 +22,29 @@ def download_images(search_query, class_name, num_images=1000):
     # Загружаем страницу поиска
     response = requests.get(search_url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    print(soup.prettify())
+
+
+    img_a = soup.find_all('a', class_='serp-item__link')
+    img_a_urls = [img_href['href'] for img_href in img_a]
 
     # Находим все ссылки на изображения
-    img_tags = soup.find_all('img', class_='serp-item__thumb')
-    img_urls = [img['src'] for img in img_tags if 'src' in img.attrs]
 
     # Счетчик для имен файлов
     file_count = 0
 
     # Загружаем изображения и сохраняем их
-    for img_url in img_urls:
+    for a in img_a_urls:
         if file_count >= num_images:
             break
 
         try:
+            search_url_link = f'https://yandex.ru{a}'
+            response_image = requests.get(search_url_link)
+            soup_image = BeautifulSoup(response_image.text, 'html.parser')
+            print(soup_image.prettify())
+
+            img_urls = soup_image.find_all('img', class_='MMImage-Origin')
+            img_url = [img['src'] for img in img_urls]
             # Проверяем, есть ли схема в URL изображения
             parsed_url = urlparse(img_url)
             if not parsed_url.scheme:
@@ -51,16 +60,17 @@ def download_images(search_query, class_name, num_images=1000):
                 with open(file_path, 'wb') as file:
                     for chunk in response.iter_content(1024):
                         file.write(chunk)
-
                 file_count += 1
+            time.sleep(0.5)
         except Exception as e:
             print(f"Ошибка при загрузке изображения: {e}")
+
 
 
 # Загружаем изображения для класса "zebra"
 download_images('zebra', 'zebra', num_images=1000)
 
 # Загружаем изображения для класса "bay horse"
-download_images('bay horse', 'bay_horse', num_images=1000)
+download_images('bay_horse', 'bay_horse', num_images=1000)
 
 print("Загрузка изображений завершена.")
